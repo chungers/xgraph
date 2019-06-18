be9f15e4a77f899033c6ef37b661eeeb19cee873
David C <davidc616+github_chungers@gmail.com>
2019-06-18 06:54:12 +0000

    prototype prints out expression
    Signed-off-by: David C <davidc616+github_chungers@gmail.com>

--- compile_test.go
+++ compile_test.go
@@ -2,14 +2,34 @@ package xgraph // import "github.com/orkestr8/xgraph"
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+func testOrderByContextIndex(a, b Edge) bool {
+	if a.To().NodeKey() != b.To().NodeKey() {
+		return false
+	}
+	ca := a.Context()
+	cb := b.Context()
+	if len(ca) == 0 && len(cb) == 0 {
+		return false
+	}
+	idx, ok := ca[0].(int)
+	if ok {
+		idx2, ok2 := cb[0].(int)
+		if ok2 {
+			return idx < idx2
+		}
+	}
+	return false
+}
+
 func TestCompileExec(t *testing.T) {
 
-	// Ratio(Sum(x1, x2, x3), Sum(y1, y2, x3))
+	// Ratio(Sum(x1, x2, x3), Sum(x3, y1, y2))
 	x1 := &nodeT{id: "x1"}
 	x2 := &nodeT{id: "x2"}
 	x3 := &nodeT{id: "x3"}
@@ -24,12 +44,12 @@ func TestCompileExec(t *testing.T) {
 	g := Builder(Options{})
 	g.Add(x1, x2, x3, y1, y2, sumX, sumY, ratio)
 
-	g.Associate(x1, input, sumX)
-	g.Associate(x2, input, sumX)
-	g.Associate(x3, input, sumX)
-	g.Associate(y1, input, sumY)
-	g.Associate(y2, input, sumY)
-	g.Associate(x3, input, sumY)
+	g.Associate(x1, input, sumX, 0)
+	g.Associate(x2, input, sumX, 1)
+	g.Associate(x3, input, sumX, 2)
+	g.Associate(y1, input, sumY, 2)
+	g.Associate(y2, input, sumY, 1)
+	g.Associate(x3, input, sumY, 0)
 	g.Associate(sumX, input, ratio, 0) // context is the positional arg index
 	g.Associate(sumY, input, ratio, 1)
 
@@ -39,7 +59,7 @@ func TestCompileExec(t *testing.T) {
 
 	ctx := context.Background()
 
-	futures := map[Edge]Awaitable{}
+	futures := map[string]Awaitable{}
 
 	var dag Awaitable
 
@@ -48,34 +68,19 @@ func TestCompileExec(t *testing.T) {
 		to := EdgeSlice(g.To(input, flow[i]).Edges())
 		from := EdgeSlice(g.From(flow[i], input).Edges())
 
-		t.Log("CUR", flow[i], "IN=", to, "OUT=", from)
-
 		// Sort the edges by context[0]
-		SortEdges(to, func(a, b Edge) bool {
-			if a.To().NodeKey() != b.To().NodeKey() {
-				return false
-			}
-			ca := a.Context()
-			cb := b.Context()
-			if len(ca) == 0 && len(cb) == 0 {
-				return false
-			}
-			idx, ok := ca[0].(int)
-			if ok {
-				idx2, ok2 := cb[0].(int)
-				if ok2 {
-					return idx < idx2
-				}
-			}
-			return false
-		})
+		SortEdges(to, testOrderByContextIndex)
 
 		input := []Awaitable{}
 		for _, in := range to {
-			if f, has := futures[in]; has {
+			if f, has := futures[fmt.Sprintf("%v", in)]; has {
 				input = append(input, f)
 			}
 		}
+
+		t.Log("COMPILE STEP", flow[i], "IN=", to, "OUT=", from, "INPUT=", input)
+
+		this := flow[i]
 
 		f := Async(ctx,
 			func() (interface{}, error) {
@@ -90,17 +95,25 @@ func TestCompileExec(t *testing.T) {
 					}
 				}
 
-				// call the actual function
+				// t.Log("RUNNING - CUR", flow[i], "IN=", to, "OUT=", from, "ARGS=", args)
 
-				return nil, nil
+				// Call the actual function
+				// Just print the operator
+				out := fmt.Sprintf("%v", this.NodeKey())
+				if len(args) > 0 {
+					out = fmt.Sprintf("%v( %v )", this.NodeKey(), args)
+				}
+
+				return out, nil
 			})
 
 		for _, out := range from {
-			futures[out] = f
+			futures[fmt.Sprintf("%v", out)] = f
 		}
 
 		dag = f
 	}
 
-	t.Log("awaitable=", dag)
+	t.Log("result=", dag.Value())
+	require.Equal(t, "ratio( [sumX( [x1 x2 x3] ) sumY( [x3 y2 y1] )] )", dag.Value())
 }
