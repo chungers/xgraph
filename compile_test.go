242bf62eb97455e1b4e2156a7b402c88132ed7d5
David C <davidc616+github_chungers@gmail.com>
2019-06-19 06:50:09 +0000

    improve prototype
    Signed-off-by: David C <davidc616+github_chungers@gmail.com>

--- compile_test.go
+++ compile_test.go
@@ -54,11 +54,12 @@ func TestCompileExec(t *testing.T) {
 	require.NoError(t, err)
 	t.Log(flow)
 
-	futures := map[Edge]Awaitable{}
-	flowStart := []chan<- interface{}{}
 	flowInput := map[Node]chan<- interface{}{}
 	flowOperators := []*future{}
 
+	{
+	}
+	futures := map[Edge]Awaitable{}
 	for i := range flow {
 
 		this := flow[i]
@@ -77,22 +78,15 @@ func TestCompileExec(t *testing.T) {
 		}
 
 		// No input means this is a Source node whose computation will be input to others
-		var startChan, inputChan chan interface{}
+		var inputChan chan interface{}
 		if len(input) == 0 {
 			inputChan = make(chan interface{}, 1)
 			flowInput[this] = inputChan
-
-			startChan = make(chan interface{})
-			flowStart = append(flowStart, startChan)
 		}
 
 		t.Log("COMPILE STEP", this, "IN=", to, "OUT=", from, "INPUT=", input)
 
 		f := newFuture(func() (interface{}, error) {
-
-			if startChan != nil {
-				<-startChan
-			}
 
 			// Given input in array of awaitable...
 			args := []interface{}{}
@@ -133,11 +127,11 @@ func TestCompileExec(t *testing.T) {
 	// Now we've built the graph.  Execute it.
 	done := make(chan interface{})
 	go func() {
-		t.Log("result=", flowOperators[len(flowOperators)-1].Value())
+		for i := range flowOperators {
+			t.Log("result[", i, "] = ", flowOperators[i].Value())
+		}
 		close(done)
 	}()
-
-	require.Equal(t, 5, len(flowStart))
 
 	ctx := context.Background()
 
@@ -146,25 +140,22 @@ func TestCompileExec(t *testing.T) {
 		flowOperators[i].doAsync(ctx)
 	}
 
-	// Give signal to start
-	for i := range flowStart {
-		close(flowStart[i])
-	}
-
 	// Set the input
 	require.Equal(t, 5, len(flowInput))
-	require.NotNil(t, flowInput[x1])
-	require.NotNil(t, flowInput[x2])
-	require.NotNil(t, flowInput[x3])
-	require.NotNil(t, flowInput[y1])
-	require.NotNil(t, flowInput[y2])
+	for _, n := range []Node{x1, x2, x3, y1, y2} {
+		require.NotNil(t, flowInput[n])
+	}
 
 	// Idea: take a map and set the values accordingly
-	flowInput[x1] <- "x1v"
-	flowInput[x2] <- "x2v"
-	flowInput[x3] <- "x3v"
-	flowInput[y1] <- "y1v"
-	flowInput[y2] <- "y2v"
+	for k, v := range map[Node]interface{}{
+		x1: "x1v",
+		x2: "x2v",
+		x3: "x3v",
+		y1: "y1v",
+		y2: "y2v",
+	} {
+		flowInput[k] <- v
+	}
 
 	<-done
 
