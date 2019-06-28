2054c5c795c482c94a4af3818a91c7dccfdc4b0c
David Chung <davidc616+github_chungers@gmail.com>
2019-06-27 18:06:25 -0700

    Refactoring node/edge rendering of dotfile; Decoding of dotfile (#12)
    * Refactor node and edge during rendering of dotfile to
    use wrapper structs (dotNode, dotEdge) which contain
    labelers for customization of dotfile rendering behavior.
    
    Implemented dotfile decoding.
    
    Signed-off-by: David C <davidc616+github_chungers@gmail.com>
    
    * proper use of dotEdge
    
    Signed-off-by: David C <davidc616+github_chungers@gmail.com>

--- edge.go
+++ edge.go
@@ -1,69 +1,29 @@
 package xgraph // import "github.com/orkestr8/xgraph"
 
 import (
-	"strings"
-
 	gonum "gonum.org/v1/gonum/graph"
-	"gonum.org/v1/gonum/graph/encoding"
 )
 
 type edge struct {
-	gonum.Edge
+	gonum   gonum.Edge
 	from    Node
 	to      Node
 	kind    EdgeKind
 	context []interface{}
-
-	labeler EdgeLabeler
 }
 
-func (e *edge) label() string {
-	if e.labeler != nil {
-		return e.labeler(&edgeView{e})
-	}
-
-	labels := []string{}
-	for i := range e.context {
-
-		switch v := e.context[i].(type) {
-		case func(Edge) string:
-			labels = append(labels, v(&edgeView{e}))
-		case EdgeLabeler:
-			labels = append(labels, v(&edgeView{e}))
-		}
-
-	}
-	return strings.Join(labels, ",")
-}
-
-func (e *edge) Attributes() []encoding.Attribute {
-	attr := attributes{}
-	if l := e.label(); l != "" {
-		attr["label"] = l
-	}
-	return attr.Attributes()
-}
-
-// edgeView is used to work around the problem that gonum.Edge.From() and xgraph.Edge.From() can't
-// be disambiguated by the compiler (different return types).  Also, we want to restrict the
-// properties exposed by separating the api implementation from the low-level implementations
-// like dot.Attributer.
-type edgeView struct {
-	*edge
-}
-
-func (e *edgeView) To() Node {
+func (e *edge) To() Node {
 	return e.to
 }
 
-func (e *edgeView) From() Node {
+func (e *edge) From() Node {
 	return e.from
 }
 
-func (e *edgeView) Context() []interface{} {
+func (e *edge) Context() []interface{} {
 	return e.context
 }
 
-func (e *edgeView) Kind() EdgeKind {
+func (e *edge) Kind() EdgeKind {
 	return e.kind
 }
